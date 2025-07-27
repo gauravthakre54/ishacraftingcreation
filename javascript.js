c4fe01f3ba6baae834d1419e57dde447205d898a
Gaurav Thakre <gauravthakre544@gmail.com>
2025-07-27 22:51:58 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// This is a JavaScript file that contains some basic functionality for the Iishas Crafting Creation website.
+let image = true;
+if (image) {
+console.log("this is an image");
+}
+alert("Welcome to Isha's Crafting Creation! share your love for handmade crafts with us. Visit our website for more details.");
